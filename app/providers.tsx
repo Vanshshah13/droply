e2faf829eff447648a37209ca2a24e0558743fc9
Vanshshah13 <shahvansh848@gmail.com>
2026-04-23 14:50:57 +0530

    fix providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -15,21 +15,10 @@ export interface ProviderProps {
 }
 
 
-const authenticator = async () => {
-    try {
-        const response = await fetch("/api/imagekit-auth");
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Authentication Error : ", error);
-        throw error;
-    }
-}
 
 export function Providers({ children, themeProps }: ProviderProps) {
     return (
-        <ImageKitProvider authenticator={authenticator}
-            publicKey={process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || ""}
+        <ImageKitProvider
             urlEndpoint={process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || ""}
         >
             <NextThemesProvider {...themeProps}>
